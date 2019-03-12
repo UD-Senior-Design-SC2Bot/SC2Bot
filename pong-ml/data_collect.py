5015660955a818aa5f51bd8ecac8157605c6d3a1
vyong1 <vyong@udel.edu>
2019-03-12 15:30:54 -0400

    Cleaned up serialization

--- pong-ml/data_collect.py
+++ pong-ml/data_collect.py
@@ -3,6 +3,9 @@ import os
 import pickle
 import time
 
+
+data_dir = os.path.join(os.getcwd(), "collected_data")
+
 class FrameData():
     def __init__(self, frame_no, input_opcode, bar_y, ball_x, ball_y):
         '''
@@ -20,25 +23,24 @@ class FrameData():
     def __str__(self):
         return "{} | {} | {} | {} | {} | {}".format(self.frame_no, self.input_opcode, self.bar_y, self.ball_x, self.ball_y)
 
-def serialize_data(data):
-    current_time = time.strftime("%Y%m%d-%H%M%S")
+def generate_filename():
+    current_time = time.strftime("%m-%d-%Y %H hr - %M m - %S s")
     computer_name = os.environ['COMPUTERNAME']
-    data_dir = os.path.join(os.getcwd(), "collected_data")
-    
+    filename = os.path.join(data_dir, "{} - {}.dat".format(computer_name, current_time))
+    return filename
+
+def serialize_data(data):
     if (not os.path.exists(data_dir)):
         os.mkdir(data_dir)
-
-    filename = os.path.join(data_dir, "{} - {}.dat".format(computer_name, current_time))
     
-    f = open(filename, "wb+")
+    f = open(generate_filename(), "wb+")
     pickled_string = pickle.dumps(data)
     f.write(pickled_string)
     f.close()
-
-
 
 def deserialize_data(filename):
     f = open(filename, "rb")
     data = f.read
     f.close()
+
 
