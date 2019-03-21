c269aab7019d43f4c5fda00a9d18a91caccc4549
vyong <vyong@udel.edu>
2019-03-21 18:00:37 -0400

    More modular data_collect.py

--- pong-ml/data_collect.py
+++ pong-ml/data_collect.py
@@ -26,7 +26,7 @@ class FrameData():
     def __str__(self):
         return "{} | {} | {} | {} | {}".format(self.frame_no, self.input_opcode, self.bar_y, self.ball_x, self.ball_y)
 
-def generate_filename():
+def generate_unique_filename():
     current_time = time.strftime("%m-%d-%Y %H hr - %M m - %S s")
     computer_name = os.environ['COMPUTERNAME']
     filename = os.path.join(data_dir, "{} - {}.dat".format(computer_name, current_time))
@@ -36,9 +36,14 @@ def serialize_data(data):
     if (not os.path.exists(data_dir)):
         os.mkdir(data_dir)
     
-    with open(generate_filename(), "wb+") as session_data_file:
+    with open(generate_unique_filename(), "wb+") as session_data_file:
         pickle.dump(data, session_data_file)
 
+def deserialize_data(filename):
+    data = None
+    with open(filename, "rb") as data_file:
+        data = pickle.load(data_file)
+    return data
 
 def deserialize_all_data():
     all_data = []
@@ -49,16 +54,21 @@ def deserialize_all_data():
 
     for data_filename in os.listdir(data_dir):
         if data_filename.endswith(".dat"):
-            with open(os.path.join(data_dir, data_filename), "rb") as data_file:
-                data = pickle.load(data_file)
-                all_data.append(data)
+            data = deserialize_data(os.path.join(data_dir, data_filename))
+            all_data.append(data)
 
     if (len(all_data) == 0):
         raise Exception("No data found")
     
     return all_data
 
+def print_dataset(dataset):
+    for frame in dataset:
+        print(frame)
+
 def print_all_data(data):
     for dataset in data:
-        for frame in dataset:
-            print(frame)
+        print_dataset(dataset)
+
+data = deserialize_data(os.path.join(os.getcwd(), "collected_data", "special", "up-only.dat"))
+print_dataset(data)
