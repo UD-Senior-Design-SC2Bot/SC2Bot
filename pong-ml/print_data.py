b5fe884e0bb3c0986b4b814c501712682d2d6067
vyong1 <vyong@udel.edu>
2019-03-25 19:50:25 -0400

    Removed unnecessary file

--- pong-ml/print_data.py
+++
@@ -1,6 +0,0 @@
-import pickle
-print("Hi!")
-with open("collected_data//FRANKHULMES-PC - 03-25-2019 14 hr - 41 m - 31 s.dat", "rb") as f:
-    data = pickle.load(f)
-    for frame in data:
-        print(frame)
