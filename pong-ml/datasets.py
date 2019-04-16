28c2f720ab5133cc671e28102663c00314146c5b
vyong <vyong@udel.edu>
2019-04-16 14:14:54 -0400

    Cleaned up directory naming

--- pong-ml/datasets.py
+++ pong-ml/datasets.py
@@ -1,14 +1,17 @@
 import data_collect
+import projdirs
 import os
 
 def load(name):
     if (name == 'up-only'):
-        dataset = data_collect.deserialize_data(os.path.join(os.getcwd(), "collected_data", "special", "up-only.dat"))
+        dataset = data_collect.deserialize_data(os.path.join(projdirs.data_special, "up-only.dat"))
     elif (name == 'down-only'):
-        dataset = data_collect.deserialize_data(os.path.join(os.getcwd(), "collected_data", "special", "down-only.dat"))
+        dataset = data_collect.deserialize_data(os.path.join(projdirs.data_special, "down-only.dat"))
     elif (name == 'no-move'):
-        dataset = data_collect.deserialize_data(os.path.join(os.getcwd(), "collected_data", "special", "no-move.dat"))
+        dataset = data_collect.deserialize_data(os.path.join(projdirs.data_special, "no-move.dat"))
     elif (name == 'all'):
         dataset = data_collect.deserialize_all_data()
 
     return dataset
+
+print(load('up-only'))
---
+++ pong-ml/projdirs.py
@@ -0,0 +1,6 @@
+import os
+
+working_dir = os.path.dirname(os.path.realpath(__file__))
+
+data = os.path.join(working_dir, 'collected_data')
+data_special = os.path.join(data, 'special')
