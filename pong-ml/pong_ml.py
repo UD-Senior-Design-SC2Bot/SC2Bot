5cd9acd88372c7f55560d1384cad5419bbe15700
vyong <vyong@udel.edu>
2019-04-18 15:21:36 -0400

    Made pong not a class again

--- pong-ml/pong_ml.py
+++ pong-ml/pong_ml.py
@@ -13,144 +13,140 @@ import matplotlib.pyplot as plt
 import data_collect
 
 
-class pong:
-    pygame.init()
-    model = ml_model.PongModel(datasets.load('ideal'))
+pygame.init()
+model = ml_model.PongModel(datasets.load('ideal'))
 
-    screen = pygame.display.set_mode((640, 480), 0, 32)
+screen = pygame.display.set_mode((640, 480), 0, 32)
 
-    # Creating 2 bars, a ball and background.
-    back = pygame.Surface((640, 480))
-    background = back.convert()
-    background.fill((0, 0, 0))
-    bar = pygame.Surface((10, 50))
-    bar1 = bar.convert()
-    bar1.fill((255, 255, 255))
-    bar2 = bar.convert()
-    bar2.fill((255, 255, 255))
-    circ_sur = pygame.Surface((15, 15))
-    circ = pygame.draw.circle(circ_sur, (255, 255, 255), ((int)(15 / 2), (int)(15 / 2)), (int)(15 / 2))
-    circle = circ_sur.convert()
-    circle.set_colorkey((0, 0, 0))
+# Creating 2 bars, a ball and background.
+back = pygame.Surface((640, 480))
+background = back.convert()
+background.fill((0, 0, 0))
+bar = pygame.Surface((10, 50))
+bar1 = bar.convert()
+bar1.fill((255, 255, 255))
+bar2 = bar.convert()
+bar2.fill((255, 255, 255))
+circ_sur = pygame.Surface((15, 15))
+circ = pygame.draw.circle(circ_sur, (255, 255, 255), ((int)(15 / 2), (int)(15 / 2)), (int)(15 / 2))
+circle = circ_sur.convert()
+circle.set_colorkey((0, 0, 0))
 
-    # some definitions
-    bar1_x, bar2_x = 10. , 620.
-    bar1_y, bar2_y = 215. , 215.
-    circle_x, circle_y = 307.5, 232.5
-    bar1_move, bar2_move = 0. , 0.
-    speed_x, speed_y, speed_circ = 250., 250., 250.
-    bar1_score, bar2_score = 0, 0
+# some definitions
+bar1_x, bar2_x = 10. , 620.
+bar1_y, bar2_y = 215. , 215.
+circle_x, circle_y = 307.5, 232.5
+bar1_move, bar2_move = 0. , 0.
+speed_x, speed_y, speed_circ = 250., 250., 250.
+bar1_score, bar2_score = 0, 0
 
-    # clock and font objects
-    clock = pygame.time.Clock()
-    font = pygame.font.SysFont("calibri", 40)
+# clock and font objects
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("calibri", 40)
 
-    # ML objects
-    session_training_data = []
+# ML objects
+session_training_data = []
 
-    frame_num = 0
-    hits = 0
+frame_num = 0
+hits = 0
 
-    done = False
+done = False
 
-    def play(self, done):
-        while done == False:
-            self.frame_num += 1
-            if self.frame_num >= 10000:
-                done = True       
-            time_passed = self.clock.tick(30)
-            time_sec = time_passed / 1000.0
-            ai_speed = self.speed_circ * time_sec
+while done == False:
+    frame_num += 1
+    if frame_num >= 10000:
+        done = True       
+    time_passed = clock.tick(30)
+    time_sec = time_passed / 1000.0
+    ai_speed = speed_circ * time_sec
 
-            # Use the model to predict the next move
-            frame_tensor = data_collect.FrameData(self.frame_num, -1, self.bar1_y, self.circle_x, self.circle_y).to_processed_tensor()
-            move = self.model.get_next_move(frame_tensor)
-            # Respond to the model
-            if (move == 1): # Up
-                self.bar1_move = -ai_speed
-            elif (move == 2): # Down
-                self.bar1_move = ai_speed
+    # Use the model to predict the next move
+    frame_tensor = data_collect.FrameData(frame_num, -1, bar1_y, circle_x, circle_y).to_processed_tensor()
+    move = model.get_next_move(frame_tensor)
+    # Respond to the model
+    if (move == 1): # Up
+        bar1_move = -ai_speed
+    elif (move == 2): # Down
+        bar1_move = ai_speed
 
-            for event in pygame.event.get():  # User did something
-                if event.type == pygame.QUIT:  # If user clicked close
-                    done = True  # Flag that we are done so we exit this loop
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        
+    score1 = font.render(str(bar1_score), True, (255, 255, 255))
+    score2 = font.render(str(bar2_score), True, (255, 255, 255))
+
+    screen.blit(background, (0, 0))
+    frame = pygame.draw.rect(screen, (255, 255, 255), Rect((5, 5), (630, 470)), 2)
+    middle_line = pygame.draw.aaline(screen, (255, 255, 255), (330, 5), (330, 475))
+    screen.blit(bar1, (bar1_x, bar1_y))
+    screen.blit(bar2, (bar2_x, bar2_y))
+    screen.blit(circle, (circle_x, circle_y))
+    screen.blit(score1, (250., 210.))
+    screen.blit(score2, (380., 210.))
+
+    bar1_y += bar1_move
+        
+    # movement of circle
+    
+        
+    circle_x += speed_x * time_sec
+    circle_y += speed_y * time_sec
+    
+    
+    # AI of the computer.
+    if circle_x >= 305.:
+        if not bar2_y == circle_y + 7.5:
+            if bar2_y < circle_y + 7.5:
+                bar2_y += ai_speed
+            if  bar2_y > circle_y - 42.5:
+                bar2_y -= ai_speed
+        else:
+            bar2_y == circle_y + 7.5
+    
+    # Safety out of bound reset
+    if bar1_y >= 420.:
+        bar1_y = 420.
+    elif bar1_y <= 10. :
+        bar1_y = 10.
+    if bar2_y >= 420.:
+        bar2_y = 420.
+    elif bar2_y <= 10.:
+        bar2_y = 10.
+        
+    # Simple Bar collision
+    if circle_x <= bar1_x + 10.:
+        if circle_y >= bar1_y - 7.5 and circle_y <= bar1_y + 42.5:
+            circle_x = 20.
+            speed_x = -speed_x
+            hits += 1
+    if circle_x >= bar2_x - 15.:
+        if circle_y >= bar2_y - 7.5 and circle_y <= bar2_y + 42.5:
+            circle_x = 605.
+            speed_x = -speed_x
                 
-            score1 = self.font.render(str(self.bar1_score), True, (255, 255, 255))
-            score2 = self.font.render(str(self.bar2_score), True, (255, 255, 255))
+    # Detects North South bounds
+    if circle_y <= 10.:
+        speed_y = -speed_y
+        circle_y = 10.
+    elif circle_y >= 457.5:
+        speed_y = -speed_y
+        circle_y = 457.5
+    
+    # Win condition.
+    if circle_x < 5.:
+        bar2_score += 1
+        circle_x, circle_y = 320., 232.5
+        bar1_y, bar_2_y = 215., 215.
+    elif circle_x > 620.:
+        bar1_score += 1
+        circle_x, circle_y = 307.5, 232.5
+        bar1_y, bar2_y = 215., 215.
+    
+    # Stop movement
+    bar1_move = 0.
+    pygame.display.update()
 
-            self.screen.blit(self.background, (0, 0))
-            frame = pygame.draw.rect(self.screen, (255, 255, 255), Rect((5, 5), (630, 470)), 2)
-            middle_line = pygame.draw.aaline(self.screen, (255, 255, 255), (330, 5), (330, 475))
-            self.screen.blit(self.bar1, (self.bar1_x, self.bar1_y))
-            self.screen.blit(self.bar2, (self.bar2_x, self.bar2_y))
-            self.screen.blit(self.circle, (self.circle_x, self.circle_y))
-            self.screen.blit(score1, (250., 210.))
-            self.screen.blit(score2, (380., 210.))
-
-            self.bar1_y += self.bar1_move
-                
-            # movement of circle
-            
-                
-            self.circle_x += self.speed_x * time_sec
-            self.circle_y += self.speed_y * time_sec
-            
-            
-            # AI of the computer.
-            if self.circle_x >= 305.:
-                if not self.bar2_y == self.circle_y + 7.5:
-                    if self.bar2_y < self.circle_y + 7.5:
-                        self.bar2_y += ai_speed
-                    if  self.bar2_y > self.circle_y - 42.5:
-                        self.bar2_y -= ai_speed
-                else:
-                    self.bar2_y == self.circle_y + 7.5
-            
-            # Safety out of bound reset
-            if self.bar1_y >= 420.:
-                self.bar1_y = 420.
-            elif self.bar1_y <= 10. :
-                self.bar1_y = 10.
-            if self.bar2_y >= 420.:
-                self.bar2_y = 420.
-            elif self.bar2_y <= 10.:
-                self.bar2_y = 10.
-                
-            # Simple Bar collision
-            if self.circle_x <= self.bar1_x + 10.:
-                if self.circle_y >= self.bar1_y - 7.5 and self.circle_y <= self.bar1_y + 42.5:
-                    self.circle_x = 20.
-                    self.speed_x = -self.speed_x
-                    self.hits += 1
-            if self.circle_x >= self.bar2_x - 15.:
-                if self.circle_y >= self.bar2_y - 7.5 and self.circle_y <= self.bar2_y + 42.5:
-                    self.circle_x = 605.
-                    self.speed_x = -self.speed_x
-                        
-            # Detects North South bounds
-            if self.circle_y <= 10.:
-                self.speed_y = -self.speed_y
-                self.circle_y = 10.
-            elif self.circle_y >= 457.5:
-                self.speed_y = -self.speed_y
-                self.circle_y = 457.5
-            
-            # Win condition.
-            if self.circle_x < 5.:
-                self.bar2_score += 1
-                self.circle_x, self.circle_y = 320., 232.5
-                self.bar1_y, self.bar_2_y = 215., 215.
-            elif self.circle_x > 620.:
-                self.bar1_score += 1
-                self.circle_x, self.circle_y = 307.5, 232.5
-                self.bar1_y, self.bar2_y = 215., 215.
-            
-            # Stop movement
-            self.bar1_move = 0.
-            pygame.display.update()
-
-game = pong()
-game.play(game.done)
-accuracy = game.hits/(game.hits + game.bar2_score)
-print("Hits: ", game.hits, " Score 1: ", game.bar1_score, " Score 2: ", game.bar2_score, "Accuracy: ", accuracy)
+accuracy = hits/(hits + bar2_score)
+print("Hits: ", hits, " Score 1: ", bar1_score, " Score 2: ", bar2_score, "Accuracy: ", accuracy)
 pygame.quit()
